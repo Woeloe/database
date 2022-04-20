7e07c44569abebf34b3d6fe5b99d4f8b162ca8fb
Woeloe <Sennewolle@hotmail.com>
2022-04-20 14:44:42 +0200

    first commit

---
+++ glenn.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, render_template, request, flash
+from main import *
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if(request.form.get("login") == "Login"):
+        naam = request.form.get("naam")
+        return redirect(f"/{naam}")
+    if(request.form.get("create") == "Create"):
+        naam = request.form.get("naam")
+        createUser(naam)
+    return render_template("index.html")
+
+@app.route("/<naam>", methods=["POST", "GET"])
+def user(naam):
+    pics = download(naam)
+    if(request.form.get("upload") == "uploaden"):
+        path = request.files["image"]
+        print(path)
+        print(path.filename)
+        print(path.name)
+        upload(path, naam)
+        return redirect(f"/{naam}")
+    
+    if(request.form.get("delete") is not None):
+        url = request.form.get("url")
+        delete(url, naam)
+        return redirect(f"/{naam}")
+    return render_template("user.html", naam = naam , pics = pics)
+
+if __name__ == "__main__":
+    app.run(debug=True)
